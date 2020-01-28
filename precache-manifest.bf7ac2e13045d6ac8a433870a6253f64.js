59ee7b53d849e87b4ef590a826cf0de221cbac25
Sanket Mohapatra <sanketmohapatra13@gmail.com>
2020-01-28 22:12:58 +0530

    Updates

---
+++ precache-manifest.bf7ac2e13045d6ac8a433870a6253f64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80eb01382a6789a6eec75dbc44eb02f",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "76c332166d2f614081ff",
+    "url": "/monster-app/static/css/main.1541f042.chunk.css"
+  },
+  {
+    "revision": "815b60ad9b576a6df034",
+    "url": "/monster-app/static/js/2.df2da0bd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-app/static/js/2.df2da0bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76c332166d2f614081ff",
+    "url": "/monster-app/static/js/main.ac1de964.chunk.js"
+  },
+  {
+    "revision": "656fc2a7e4394c8ed5a2",
+    "url": "/monster-app/static/js/runtime-main.c8392871.js"
+  }
+]);
